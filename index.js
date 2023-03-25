c8ad6528f4779e1d3eac1f7f161d82db9942ae8c
Mike Massie <mike@michaelmassie.com>
2023-03-25 14:15:36 -0500

    Testing a autogenerated ChatGPT variant to start

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-import axios, { AxiosResponse } from 'axios';
-import { parse, HTMLElement } from 'node-html-parser';
+const https = require('https');
+const { parse } = require('node-html-parser');
 
-async function fetch(): Promise<AxiosResponse> {
-  try {
-    return axios.get('https://www.bbc.co.uk/news/business/market-data');
-  } catch (error: any) {
-    throw new Error(error);
-  }
-}
+exports.handler = async (event) => {
+  const url = 'https://adventurerock.com/';
 
-async function getMarkets(): Promise<Market[]> {
-  const TABLE_CLASS: string = '.nw-c-md-overview-table__row';
-  const html: AxiosResponse = await fetch();
-  const root = parse(html.data);
-
-  const markets = [];
-
-  for (let i = 0; i < 13; i++) {
-    const row: HTMLElement = root.querySelectorAll(TABLE_CLASS)[i];
-    const name: string = row
-      .querySelector('.nw-c-md-overview-table__link')
-      .querySelector('span').text;
-
-    const percentChange: string = root
-      .querySelectorAll(TABLE_CLASS)
-      [i].querySelectorAll('.nw-c-md-overview-table__cell-inner')[1].text;
-
-    const price: number = root
-      .querySelectorAll(TABLE_CLASS)
-      [i].querySelectorAll('.nw-c-md-overview-table__cell-inner')[2].text as unknown as number;
-    const absoluteChange: number = root
-      .querySelectorAll(TABLE_CLASS)
-      [i].querySelectorAll('.nw-c-md-overview-table__cell-inner')[3].text as unknown as number;
-
-    markets.push({
-      name: name.replace(/\s/g, '').toUpperCase(),
-      price,
-      percentChange,
-      absoluteChange
+  const getPageHTML = async (url) => {
+    return new Promise((resolve, reject) => {
+      https.get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          resolve(data);
+        });
+      }).on('error', (err) => {
+        reject(err);
+      });
     });
-  }
+  };
 
-  return markets;
-}
+  const html = await getPageHTML(url);
+  const root = parse(html);
 
-export { fetch, getMarkets };
+  const locations = {
+    Brookfield: '',
+    WalkersPoint: '',
+    Downtown: ''
+  };
+
+  const occupancyElements = root.querySelectorAll('.rmOccupancy');
+
+  occupancyElements.forEach((element) => {
+    const locationName = element.parentNode.parentNode.parentNode.querySelector('.rmTitle').childNodes[0].rawText.trim();
+    if (locationName in locations) {
+      const occupancy = element.childNodes[0].rawText.trim();
+      locations[locationName] = occupancy;
+    }
+  });
+
+  console.log('Occupancy data:', locations);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(locations),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+};
