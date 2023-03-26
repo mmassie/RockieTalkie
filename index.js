e52e1001d3e2ebbd311c0f21d5d4e7858d737dc9
Mike Massie <mike@michaelmassie.com>
2023-03-26 10:34:03 -0500

    chatGPT generations wAxios

--- index.js
+++ index.js
@@ -1,51 +1,33 @@
-const https = require('https');
+const axios = require('axios');
 const { parse } = require('node-html-parser');
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const url = 'https://adventurerock.com/';
-
-  const getPageHTML = async (url) => {
-    return new Promise((resolve, reject) => {
-      https.get(url, (res) => {
-        let data = '';
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          resolve(data);
-        });
-      }).on('error', (err) => {
-        reject(err);
-      });
+  
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const root = parse(html);
+    
+    const locations = ['Brookfield', 'Walkers Point', 'Downtown'];
+    const occupancy = {};
+    
+    locations.forEach(location => {
+      const count = root.querySelector(`#${location.toLowerCase()} #count`).rawText.trim();
+      const capacity = root.querySelector(`#${location.toLowerCase()} #capacity`).rawText.trim();
+      
+      occupancy[location] = { count, capacity };
     });
-  };
-
-  const html = await getPageHTML(url);
-  const root = parse(html);
-
-  const locations = {
-    Brookfield: '',
-    WalkersPoint: '',
-    Downtown: ''
-  };
-
-  const occupancyElements = root.querySelectorAll('.rmOccupancy');
-
-  occupancyElements.forEach((element) => {
-    const locationName = element.parentNode.parentNode.parentNode.querySelector('.rmTitle').childNodes[0].rawText.trim();
-    if (locationName in locations) {
-      const occupancy = element.childNodes[0].rawText.trim();
-      locations[locationName] = occupancy;
-    }
-  });
-
-  console.log('Occupancy data:', locations);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(locations),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(occupancy)
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error scraping occupancy data' })
+    };
+  }
 };
