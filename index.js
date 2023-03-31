e10ff4cf9d1d3a026118e25d6f93e6be46f5f9ac
Mike Massie <mike@michaelmassie.com>
2023-03-31 16:19:59 -0500

    reduced back to html-parse

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-//import axios from 'axios';
-//import { parse } from 'node-html-parser';
+import { parse } from 'node-html-parser';
+import fetch from 'node-fetch';
 
-const axios = require('axios');
-const { parse } = require('node-html-parser');
-
-exports.handler = async (event, context) => {
-  const url = 'https://adventurerock.com/';
-  
-  try {
-    const response = await axios.get(url);
-    const html = response.data;
-    const root = parse(html);
-    
-    const locations = ['Brookfield', 'Walkers Point', 'Downtown'];
-    const occupancy = {};
-    
-    locations.forEach(location => {
-      const count = root.querySelector(`#${location.toLowerCase()} #count`).rawText.trim();
-      const capacity = root.querySelector(`#${location.toLowerCase()} #capacity`).rawText.trim();
-      
-      occupancy[location] = { count, capacity };
-    });
-    
-    return {
-      statusCode: 200,
-      body: JSON.stringify(occupancy)
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error scraping occupancy data' })
-    };
-  }
-
+const url = 'https://adventurerock.com';
+const gymIds = {
+  BRK: ['facility-count', 'count', 'capacity'],
+  MKE: ['facility-count', 'count', 'capacity'],
+  WKP: ['facility-count', 'count', 'capacity'],
 };
+
+fetch(url)
+  .then(res => res.text())
+  .then(html => {
+    const root = parse(html);
+        // console.log(root);
+    for (const [gym, ids] of Object.entries(gymIds)) {
+      console.log(`IDs for gym ${gym}:`);
+      for (const id of ids) {
+        const elem = root.querySelector(`#${gym}-${id}`);
+        if (elem) {
+          console.log(`- ${id}: ${elem.text}`);
+        } else {
+          console.log(`- ${id}: not found`);
+        }
+      }
+    }
+  })
+
+  .catch(err => console.error(err));
